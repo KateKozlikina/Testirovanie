bb6e1b60d0bde438e88d6335e896d520c7e5d9b4
KateKozlikina <kozlikina9511@gmail.com>
2017-05-25 23:11:26 +0700

    Create TestMileToMetr

--- ConvertTDD/Test/UnitTest1.cs
+++ ConvertTDD/Test/UnitTest1.cs
@@ -25,6 +25,15 @@ namespace Test
             var value = converter.MetrToMile(100000);
             Assert.AreEqual(62.14, value);
         }
+
+        [TestMethod]
+        public void TestMileToMetr()
+        {
+            Converter converter = new Converter();
+            var value = converter.MileToMetr(100);
+            double res = 100 / 0.0006214;
+            Assert.AreEqual(res, value);
+        }
     }
 
 }
