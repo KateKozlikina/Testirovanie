5a7bab228dd2bea9e0926a031777d6fa01a9cce3
KateKozlikina <kozlikina9511@gmail.com>
2017-05-25 22:35:08 +0700

    Create metod MetrToMile

--- ConvertTDD/ConvertTDD/Converter.cs
+++ ConvertTDD/ConvertTDD/Converter.cs
@@ -8,5 +8,12 @@ namespace ConvertTDD
 {
 public    class Converter
     {
+        static double MetrTOMile = 0.0006214;
+        static double MetrTOFt = 3.281;
+        static double MileTOFt = 5280;
+        public double MetrToMile(double metr)
+        {
+            return metr * MetrTOMile;
+        }
     }
 }
