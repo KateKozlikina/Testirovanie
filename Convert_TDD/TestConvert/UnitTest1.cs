662d78aedb8f5ed13b90c227188905eceda48ebb
KateKozlikina <kozlikina9511@gmail.com>
2017-05-25 20:59:53 +0700

    Метод футы в мили (тест)

--- Convert_TDD/TestConvert/UnitTest1.cs
+++ Convert_TDD/TestConvert/UnitTest1.cs
@@ -56,6 +56,15 @@ namespace TestConvert
             Assert.AreEqual(res, value);
         }
 
+        [TestMethod]
+        public void TestFtToMile()
+        {
+            Converter converter = new Converter();
+            var value = converter.FtToMile(100);
+            double res = 100 / 5280;
+            Assert.AreEqual(res, value);
+        }
+
 
     }
 }
