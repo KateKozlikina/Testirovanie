36f2b68c9533483bd9d1c4b4c56a7b3391e0bff4
KateKozlikina <kozlikina9511@gmail.com>
2017-05-25 16:09:06 +0700

    Создание класса Сonverter (Тест)

--- Convert_TDD/TestConvert/UnitTest1.cs
+++ Convert_TDD/TestConvert/UnitTest1.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Convert_TDD;
 namespace TestConvert
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestConvert()
         {
+            Converter converter = new Converter();
+            Assert.IsNotNull(converter);
         }
+        [TestMethod]
+        public void TestMetrToMile()
+        {
+            Converter converter = new Converter();
+            var value = converter.MetrToMile(100);
+            Assert.AreEqual(62.14, value);
+        }
+
     }
 }
