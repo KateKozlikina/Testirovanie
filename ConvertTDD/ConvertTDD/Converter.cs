1427e6e81e984766d13f1fe48885f1dbfeffa0c0
KateKozlikina <kozlikina9511@gmail.com>
2017-05-25 23:12:53 +0700

    Create metod MileToMetr

--- ConvertTDD/ConvertTDD/Converter.cs
+++ ConvertTDD/ConvertTDD/Converter.cs
@@ -15,5 +15,10 @@ public    class Converter
         {
             return metr * MetrTOMile;
         }
+
+        public object MileToMetr(int v)
+        {
+            return v / MetrTOMile;
+        }
     }
 }
